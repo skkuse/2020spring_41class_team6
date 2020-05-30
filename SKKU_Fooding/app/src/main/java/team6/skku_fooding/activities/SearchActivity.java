38cf536e1575e7f3635b77dd98b110d5b1136675
chocoPenguin <ableway98@gmail.com>
2020-05-30 18:09:05 +0900

    Modify searchactivity ADD acitivity_search_layout

--- SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/SearchActivity.java
+++ SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/SearchActivity.java
@@ -1,20 +1,201 @@
 package team6.skku_fooding.activities;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.w3c.dom.Comment;
+
+import java.util.ArrayList;
+import java.util.Map;
+
 import team6.skku_fooding.R;
 
 public class SearchActivity extends AppCompatActivity {
 
+    String search_strring;
+    Button search_button;
+    private DatabaseReference dbReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+
+        dbReference= FirebaseDatabase.getInstance().getReference().child("product");
+        ListView listview ;
+        ListViewAdapter adapter;
+
+        // Adapter 생성
+        adapter = new ListViewAdapter() ;
+
+        // 리스트뷰 참조 및 Adapter달기
+        listview = (ListView) findViewById(R.id._listView);
+        listview.setAdapter(adapter);
+
+
+
+        ChildEventListener childEventListener = new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d("TAG", "onChildAdded:" + dataSnapshot.getKey());
+
+                Map<String, Object> currentObject = (Map<String, Object>) dataSnapshot.getValue();
+                String name=currentObject.get("name").toString();
+                String price=currentObject.get("price").toString();
+
+                adapter.addItem(ContextCompat.getDrawable(SearchActivity.this,R.drawable.app_icon),
+                        name, price);
+
+                /*
+                for(String child: user.keySet()) {
+                    Map<String, Object> currentObject = (Map<String, Object>) user.get(child);
+                    String name=currentObject.get("name").toString();
+                    String price=currentObject.get("price").toString();
+
+                    adapter.addItem(ContextCompat.getDrawable(SearchActivity.this,R.drawable.app_icon),
+                            name, price);
+                }*/
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String previousChildName) {
+                Log.d("TAG", "onChildChanged:" + dataSnapshot.getKey());
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String previousChildName) {
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        };
+        dbReference.addChildEventListener(childEventListener);
+
     }
 
     @Override
     public void onBackPressed() {
     }
 
+}
+
+class ListViewItem {
+    private Drawable iconDrawable;
+    private String titleStr;
+    private String descStr;
+
+    public void setIcon(Drawable icon) {
+        iconDrawable = icon;
+    }
+
+    public void setTitle(String title) {
+        titleStr = title;
+    }
+
+    public void setDesc(String desc) {
+        descStr = desc;
+    }
+
+    public Drawable getIcon() {
+        return this.iconDrawable;
+    }
+
+    public String getTitle() {
+        return this.titleStr;
+    }
+
+    public String getDesc() {
+        return this.descStr;
+    }
+}
+
+class ListViewAdapter extends BaseAdapter {
+    // Adapter에 추가된 데이터를 저장하기 위한 ArrayList
+    private ArrayList<ListViewItem> listViewItemList = new ArrayList<ListViewItem>() ;
+
+    // ListViewAdapter의 생성자
+    public ListViewAdapter() {
+
+    }
+
+    // Adapter에 사용되는 데이터의 개수를 리턴. : 필수 구현
+    @Override
+    public int getCount() {
+        return listViewItemList.size() ;
+    }
+
+    // position에 위치한 데이터를 화면에 출력하는데 사용될 View를 리턴. : 필수 구현
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final int pos = position;
+        final Context context = parent.getContext();
+
+        // "listview_item" Layout을 inflate하여 convertView 참조 획득.
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.activity_search_layout, parent, false);
+        }
+
+        // 화면에 표시될 View(Layout이 inflate된)으로부터 위젯에 대한 참조 획득
+        ImageView iconImageView = (ImageView) convertView.findViewById(R.id.imageView1) ;
+        TextView titleTextView = (TextView) convertView.findViewById(R.id.textView1) ;
+        TextView descTextView = (TextView) convertView.findViewById(R.id.textView2) ;
+
+        // Data Set(listViewItemList)에서 position에 위치한 데이터 참조 획득
+        ListViewItem listViewItem = listViewItemList.get(position);
+
+        // 아이템 내 각 위젯에 데이터 반영
+        iconImageView.setImageDrawable(listViewItem.getIcon());
+        titleTextView.setText(listViewItem.getTitle());
+        descTextView.setText(listViewItem.getDesc());
+
+        return convertView;
+    }
+
+    // 지정한 위치(position)에 있는 데이터와 관계된 아이템(row)의 ID를 리턴. : 필수 구현
+    @Override
+    public long getItemId(int position) {
+        return position ;
+    }
+
+    // 지정한 위치(position)에 있는 데이터 리턴 : 필수 구현
+    @Override
+    public Object getItem(int position) {
+        return listViewItemList.get(position) ;
+    }
+
+    // 아이템 데이터 추가를 위한 함수. 개발자가 원하는대로 작성 가능.
+    public void addItem(Drawable icon, String title, String desc) {
+        ListViewItem item = new ListViewItem();
+
+        item.setIcon(icon);
+        item.setTitle(title);
+        item.setDesc(desc);
+
+        listViewItemList.add(item);
+    }
 }
