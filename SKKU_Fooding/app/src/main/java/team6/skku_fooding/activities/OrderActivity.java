ac848e514a3763f5e3bead374e808d23ae658b7b
WilliamJung <48509949+anjdiwkdsks@users.noreply.github.com>
2020-06-08 22:17:01 +0900

    Update OrderActivity.java

--- SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/OrderActivity.java
+++ SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/OrderActivity.java
@@ -21,7 +21,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
+import team6.skku_fooding.R;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
