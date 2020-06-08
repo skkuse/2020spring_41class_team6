6bda880ac9cd43ed07371cce7bafc9539959529a
WilliamJung <48509949+anjdiwkdsks@users.noreply.github.com>
2020-06-08 12:54:54 +0900

    Update SurveyActivity.java
    have to get Uid from signup activity

--- SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/SurveyActivity.java
+++ SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/SurveyActivity.java
@@ -14,12 +14,13 @@ import team6.skku_fooding.R;
 import team6.skku_fooding.models.Survey;
 
 public class SurveyActivity extends AppCompatActivity {
-
+    public static final String USER_ID = "survey_william.USER_ID";
     Intent intent;
     Button next_button;
     RadioGroup radioGroup;
     RadioButton radioButton;
     int checked;
+    int user_id=5858585;
     @Override
     public void onBackPressed(){
 
@@ -28,7 +29,8 @@ public class SurveyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_survey);
-
+        //주원씨한테서 user_id 받아오기
+        //int temp = intent.getIntExtra(SignupActivity.name);
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
         radioGroup = findViewById(R.id.radio_group);
@@ -47,12 +49,15 @@ public class SurveyActivity extends AppCompatActivity {
             int radioId = radioGroup.getCheckedRadioButtonId();
             radioButton=findViewById(radioId);
             if(checked==0){
+               // user_id=temp; // 주원씨 한테서 잘 받아오면 해제
                 startSurvey();
             }
             if(checked==1){
+               // user_id=temp;
                 startSurvey2();
             }
             if(checked==2){
+              //  user_id=temp;
                 startSurvey3();
             }
         });
@@ -63,29 +68,23 @@ public class SurveyActivity extends AppCompatActivity {
     }
     private void startSurvey(){
         Intent intent =new Intent(SurveyActivity.this,SurveyActivity_Spicy.class);
+        intent.putExtra(USER_ID,user_id);
         startActivity(intent);
         finish();
     }
 
     private void startSurvey2(){
         Intent intent =new Intent(SurveyActivity.this,SurveyActivity_Sweet.class);
+        intent.putExtra(USER_ID,user_id);
         startActivity(intent);
         finish();
     }
     private void startSurvey3(){
         Intent intent =new Intent(SurveyActivity.this,SurveyActivity_Salty.class);
+        intent.putExtra(USER_ID,user_id);
         startActivity(intent);
         finish();
     }
 
 }
-    /*
-
-    설문조사가 완료되지 않았으면 뒤로가기 안되게,
-    설문조사가 완료되었으면 뒤로가기 되게! (다시 login activity로 돌아올 수 있게)
-    @Override
-    public void onBackPressed() {
-
-    }
-    */
 
