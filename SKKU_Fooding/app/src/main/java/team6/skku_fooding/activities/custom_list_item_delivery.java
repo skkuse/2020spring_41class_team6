edd09067b3f243d9c7d434c7b5dcfcc27924a533
WilliamJung <48509949+anjdiwkdsks@users.noreply.github.com>
2020-06-08 12:55:54 +0900

    Update custom_list_item_delivery.java

--- SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/custom_list_item_delivery.java
+++ SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/custom_list_item_delivery.java
@@ -6,11 +6,20 @@ public class custom_list_item_delivery {
     private String order_id;
     private String date;
     private String status;
+    private String product_name;
 
-    public custom_list_item_delivery(String order_id, String date, String status) {
+    public custom_list_item_delivery(String order_id, String date, String status,String product_name) {
         this.order_id = order_id;
         this.date = date;
         this.status = status;
+        this.product_name = product_name;
+    }
+
+    public String getProduct_name() {
+        return product_name;
+    }
+    public void setProduct_name(String product_name) {
+        this.product_name = product_name;
     }
 
     public String getOrder_id() {
