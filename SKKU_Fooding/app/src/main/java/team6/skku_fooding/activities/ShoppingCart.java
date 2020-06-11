85e88c54c9a7b0269379d617eba5558faa25981a
chocoPenguin <ableway98@gmail.com>
2020-06-11 22:18:23 +0900

    Fixing error of product_detail

--- SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/ShoppingCart.java
+++ SKKU_Fooding/app/src/main/java/team6/skku_fooding/activities/ShoppingCart.java
@@ -121,7 +121,7 @@ public class ShoppingCart extends AppCompatActivity {
 
 
 
-            ShoppingAdapter adapter = new ShoppingAdapter(this, images, productnames, amount,clicked,prices,productids,shoppingcart);
+            ShoppingAdapter adapter = new ShoppingAdapter(this, images, productnames, amount,clicked,prices,productids,shoppingcart,UID);
             lv.setAdapter(adapter);
 
             selectednames=adapter.selectednameslist();
